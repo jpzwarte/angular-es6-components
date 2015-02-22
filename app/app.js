dff9bee281562d41f8f761b4ad5ab258c97ba4ce
Jeroen Zwartepoorte <jeroen.zwartepoorte@gmail.com>
2015-02-22 10:38:41 +0100

    Add fix for current angular router lack of features.

--- app/app.js
+++ app/app.js
@@ -19,12 +19,12 @@ import { DashboardController } from './components/dashboard/dashboard'
 angular.module('app.dashboard', [])
     .controller('DashboardController', DashboardController);
 
-import { ResultsController } from './components/dashboard/results/results'
+import { ResultsController } from './components/results/results'
 
 angular.module('app.dashboard.results', [])
     .controller('ResultsController', ResultsController);
 
-import { LessonsController } from './components/dashboard/lessons/lessons'
+import { LessonsController } from './components/lessons/lessons'
 
 angular.module('app.dashboard.lessons', [])
     .controller('LessonsController', LessonsController);
